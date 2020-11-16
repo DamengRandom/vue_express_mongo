a9f75fab64d76fdafe5948d2b6b40cf744526a77
Damon Wu <damonwu0605@gmail.com>
2020-11-16 18:03:19 +1100

    Revert "add docker settings" for git recall only
    This reverts commit 8e8adaf119f62f9db012cf262322fec0eb916162.

--- client/vue.config.js
+++ client/vue.config.js
@@ -6,8 +6,7 @@ module.exports = {
   devServer: {
     proxy: { 
       '/api': {
-        // target: 'http://localhost:4940' // docker way port is 4940
-        target: 'http://localhost:4932' // heroku is reading port as 4932 && docker-compose way of using 4932
+        target: 'http://localhost:4932'
       }
     }
   }
